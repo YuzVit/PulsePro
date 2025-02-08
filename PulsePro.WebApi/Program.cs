990f1225530323e2843403e599add7d6204d8e81
VitaliiYuz <yuzvin268@gmail.com>
2025-02-08 16:42:56 +0200

    *Created base architecture folders and swagger

---
+++ PulsePro.WebApi/Program.cs
@@ -0,0 +1,22 @@
+using PulsePro.WebApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi().ConfigureWebApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.Run();
---
+++ PulsePro.WebApi/ServiceExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace PulsePro.WebApi;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureWebApi(this IServiceCollection services)
+    { 
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+}
